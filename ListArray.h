c9904622919914fd3a85c33bdf124474bb6a44cb
Joan Albert Silvestre <jsilvestre@dsic.upv.es>
2024-09-24 20:24:19 +0200

    Añadida implementación de la calse ListArray

---
+++ ListArray.h
@@ -0,0 +1,23 @@
+#include <ostream>
+#include "List.h"
+
+template <typename T>
+class ListArray : public List<T>{
+
+	private:
+	T* arr;
+	int max;
+	int n;
+	static const int MINSIZE;
+
+	public:
+	
+	ListArray();
+	~ListArray();
+	T operator[](int pos);
+	friend std::oeastream& operator<<(std::ostream &out, const ListArray<T< &list);
+	void resize(int new_size);
+
+
+};
+
