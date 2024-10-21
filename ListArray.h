c1874b698ac7607c18d7b13811fdb40010b470b4
Joan Albert Silvestre <jsilvestre@dsic.upv.es>
2024-10-21 14:23:53 +0200

    Añadida implementación de la calse ListArray

--- ListArray.h
+++ ListArray.h
@@ -5,19 +5,59 @@ template <typename T>
 class ListArray : public List<T>{
 
 	private:
-	T* arr;
-	int max;
-	int n;
-	static const int MINSIZE;
+	T* arr;//Puntero al array dianmico
+	int max;//Capacidad del array
+	int n; //Numero actural de elementos lista
+	static const int MINSIZE;//Tamaño min del array
+
+	//Metodo para redimensionar el array dinamico	
+	void resize(int new_size){
+        if (new_size <= max){
+                return; //Si nuevo tamaño es menor o igual al actual, no hacemos nada
+        }
+
+        T* new_arr = new T[new_size];//Reservamos nueva memoria
+        for (int i = 0;i <n;i++){
+                new_arr[i] = arr[i]; //Copiamos los elem en el nuevo array
+        }
+
+        delete[] arr; //liberamos memoria del antiguo array
+        arr = new_arr; // Puntero apunta nuevo array
+        max = new_size; //Actualizamos cap max
+        }
+
+	
+//=========================================
 
 	public:
 	
-	ListArray();
-	~ListArray();
-	T operator[](int pos);
-	friend std::oeastream& operator<<(std::ostream &out, const ListArray<T< &list);
-	void resize(int new_size);
+	//COnstructor defecto
+	ListArray(){
+	max = MINSIZE;
+	arr = new T[max];
+	n =0;
+	}
+//==========================================
+	~ListArray(){
+		delete[]arr;
+	}
+//==========================================
+	T operator[](int pos){
+		if (pos < 0 || pos >=n){
+			throw std::out_of_range("Index out of bounds");
+		}
+		return arr[pos];
+	}
+//==========================================
+	friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
+	for (int i=0; i< list.n;i++){
+		out<< list.arr[i] << " ";//Imprime cada elemento
+	}
+	return out;
+	}
 
 
 };
+
+
 
