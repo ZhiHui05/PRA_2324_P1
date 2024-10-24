dd4381e187b4f2d323c927e4010b0953dbecd583
ZhiHui05 <zli14z@upv.edu.es>
2024-10-24 13:47:28 +0200

    probandoPat

--- ListLinked.h
+++ ListLinked.h
@@ -30,6 +30,13 @@
 		n = 0;
 	}
 
+	// Método para agregar nodos al principio de la lista
+   	void prepend(T value) override {
+        Node<T>* newNode = new Node<T>(value, first);
+        first = newNode;
+        ++n;
+    }
+
 	T operator[](int pos){
 		if ( pos < 0 || pos >= n){
 		throw std::out_of_range("Valor fuera del intervalo");
@@ -46,7 +53,104 @@
 	
 	friend std::ostream& operator<<(std::ostream& out, const ListLinked<T>& list);
 
-	};
+//===================================================================================
+
+
+	// Implementación de métodos de la clase base List<T>
+    	void insert(int pos, T e) override {
+        if (pos < 0 || pos > n) {
+            throw std::out_of_range("Índice fuera del intervalo");
+        }
+        if (pos == 0) {
+            prepend(e);
+        } else {
+            Node<T>* current = first;
+            for (int i = 0; i < pos - 1; ++i) {
+                current = current->next;
+            }
+            Node<T>* newNode = new Node<T>(e, current->next);
+            current->next = newNode;
+            ++n;
+		}
+   	
+   	}
+//=================================================================================
+
+    	void append(T e) override {
+        insert(n, e);  // Insertar al final es lo mismo que insertar en la posición n
+    	}
+//================================================================================
+
+    	T remove(int pos) override {
+        	if (pos < 0 || pos >= n) {
+           	throw std::out_of_range("Índice fuera del intervalo");
+        	}
+        	Node<T>* current = first;
+        	T removedData;
+        	if (pos == 0) {
+            		removedData = first->data;
+            		first = first->next;
+            	delete current;
+        	} else {
+            		for (int i = 0; i < pos - 1; ++i) {
+                	current = current->next;
+            	}
+            	Node<T>* nodeToRemove = current->next;
+            	removedData = nodeToRemove->data;
+            	current->next = nodeToRemove->next;
+            	delete nodeToRemove;
+        	}
+
+        	--n;
+        	return removedData;
+    		
+	}	
+//=========================================================
+
+	T get(int pos) const override {
+        if (pos < 0 || pos >= n) {
+            throw std::out_of_range("La posición está fuera del rango.");
+        }
+	Node<T>* current = first;
+
+        for (int i = 0;i < pos; i++){
+		current = current -> next;
+	}
+
+        return current->data;
+        }
+
+
+//=========================================================
+    	bool empty() const override {
+        	return n == 0;
+    	}	
+
+	
+//========================================================
+	int size() const override {
+        	return n;
+    	}
+//==========================================================
+    	int search(T e) const override {
+        	Node<T>* current = first;
+        	int index = 0;
+        	while (current != nullptr) {
+            		if (current->data == e) {
+                	return index;
+            	}
+            	
+		current = current->next;
+            	index++;
+        	}	
+        	
+		return -1;
+    	
+	}
+
+
+
+};
 
 		template<typename T>
    	 	std::ostream& operator<<(std::ostream& out, const ListLinked<T>& list) {
